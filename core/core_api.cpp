7cf786f5c56e4ab2d834ab52a4b408af3438e3d8
Livire <playroooook@gmail.com>
2026-01-04 11:46:30 +0300

    Update

--- core/core_api.cpp
+++ core/core_api.cpp
@@ -1,14 +1,15 @@
 extern "C"{
-	int create_account_impl(int);
-	int get_balance_impl(int);
-	int transfer_impl(int,int,int,int*,int*);
+	int create_account(int);
+	int get_balance(int);
+	int transfer(int,int,int,int*,int*);
 	int core_create_account(int initial_balance){
-		return create_account_impl(initial_balance);
+		return create_account(initial_balance);
 	}
 	int core_get_balance(int account_id){
-		return get_balance_impl(account_id);
+		return get_balance(account_id);
 	}
 	int core_transfer(int from_id,int to_id,int amount,int* new_balance_from,int* new_balance_to){
-		return transfer_impl(from_id,to_id,amount,new_balance_from,new_balance_to);
+		return transfer(from_id,to_id,amount,new_balance_from,new_balance_to);
 	}
 }
+
